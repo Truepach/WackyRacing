ea6f9cb8708fc8efd5265940c3725a3f89dca7be
Truepach <siadodigger@gmail.com>
2023-02-08 12:44:59 +1000

    GameBeta1
    Gamepllay in general done. pre commit before TitleScreen branch

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     public GameObject platformSpawner;
+    public GameObject gamePlayUi;
     public bool isGameStarted = false;
+
+    public TMPro.TextMeshProUGUI scoreText;
+
+    int score = 0;
     private void Awake()
     {
         if(instance == null) 
@@ -17,7 +25,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -36,11 +44,31 @@ public class GameManager : MonoBehaviour
     {
         isGameStarted = true;
         platformSpawner.SetActive(true);
+        gamePlayUi.SetActive(true);
+        StartCoroutine(UpdateScore());
     }
 
     public void GameOver()
     {
         platformSpawner.SetActive(false);
         isGameStarted = false;
+        Invoke("ReloadLevel", 1f);
+    }
+
+    void ReloadLevel()
+    {
+        SceneManager.LoadScene("GameRun");
+    }
+
+    IEnumerator UpdateScore()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+            score++;
+            scoreText.text = score.ToString();
+
+
+        }
     }
 }
